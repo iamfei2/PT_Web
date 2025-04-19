f9464ee848b6514b3f3d6ff6074efa4ec0604948
iamfei2 <1298333784@qq.com>
2025-04-19 14:34:11 +0800

    框架

---
+++ Pt_Back/src/main/java/org/stuinfo/pt_back/PtBackApplication.java
@@ -0,0 +1,13 @@
+package org.stuinfo.pt_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PtBackApplication.class, args);
+    }
+
+}
---
+++ Pt_Back/src/test/java/org/stuinfo/pt_back/PtBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.stuinfo.pt_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
